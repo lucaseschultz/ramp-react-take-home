f562310d46c98736b670ab57f8aa33bb00375ffc
Lucas Schultz <156949026+lucaseschultz@users.noreply.github.com>
2024-08-29 15:15:43 +0000

    updated stylesheet name and replaced template with components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import "./styles.css";
+import "./app.css";
+import BackgroundImage from "./components/background-img";
+import NavbarTop from "./components/navbar-top";
+import Footer from "./components/footer";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="page">
+      <BackgroundImage />
+      <NavbarTop />
+      <Footer />
     </div>
   );
 }
