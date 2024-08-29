8166dd5d53cf324d6db7995e0f9f4a04dbab4c82
Lucas Schultz <156949026+lucaseschultz@users.noreply.github.com>
2024-08-29 13:13:09 +0000

    added footer component and styling

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import './footer.css';
+
+export default function NavbarTop() {
+    return (
+        <nav className='footer-container'>
+            <div className='footer-content'>
+                <a href='https://wyomingschultz.com/resume/' target={'_blank'}>by Lucas Schultz</a>
+            </div>
+        </nav>
+    );
+}
