deabe3f79a7ea08033c2e214f695919d84fd7ad7
Lucas Schultz <156949026+lucaseschultz@users.noreply.github.com>
2024-08-29 13:17:03 +0000

    added ramp-flag component and stylesheet

---
+++ src/components/ramp-flag.tsx
@@ -0,0 +1,11 @@
+export default function RampFlag() {
+    return (
+        <div className={'flag-container'}>
+            <div className={'flag-content'}>
+                <ul className={'flag-list'}>
+
+                </ul>
+            </div>
+        </div>
+    );
+}
