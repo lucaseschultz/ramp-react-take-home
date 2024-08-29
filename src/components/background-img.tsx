64a049ab335a578a2c898c4be9740ed638f8586d
Lucas Schultz <156949026+lucaseschultz@users.noreply.github.com>
2024-08-29 15:13:31 +0000

    moved file to a location that I can extract from

--- src/components/background-img.tsx
+++ src/components/background-img.tsx
@@ -1,10 +1,11 @@
 import "./background-img.css";
+import BGImg from "../images/background-wood.jpg";
 
 export default function BackgroundImg() {
   return (
     <div className={`background-img-container`}>
       <img
-        src="../../public/background-wood.jpg"
+        src={BGImg}
         alt="Background Image"
         placeholder="blur"
         className="background-img"
