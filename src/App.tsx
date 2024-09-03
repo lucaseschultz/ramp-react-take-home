9560aa6ed4590d860e796de8b0422a60bc611221
Lucas Schultz <156949026+lucaseschultz@users.noreply.github.com>
2024-09-03 21:24:44 +0000

    Added CTFScript route to page

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import "./app.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BackgroundImage from "./components/background-img";
 import NavbarTop from "./components/navbar-top";
-import Main from "./home/home";
 import Footer from "./components/footer";
+import Main from "./home/home";
+import CTFScript from "./ctf-script/ctf-script";
 
 export default function App() {
   return (
@@ -13,7 +14,7 @@ export default function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Main />} />
-          {/* <Route path="/settings" element={SettingsPage()} /> */}
+          <Route path="/settings" element={<CTFScript />} />
         </Routes>
       </Router>
       <Footer />
---
+++ src/ctf-script/ctf-script.tsx
@@ -0,0 +1,5 @@
+import "./ctf-script.css";
+
+export default function CTFScript() {
+  return <div className="main">{/* insert script here */}</div>;
+}
