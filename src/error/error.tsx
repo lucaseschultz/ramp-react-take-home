b1b003489f4e6ba4f0c5e3929565051b4ea5ed40
Lucas Schultz <156949026+lucaseschultz@users.noreply.github.com>
2024-09-04 17:52:33 +0000

    Created error page content and styling

--- src/error/error.tsx
+++ src/error/error.tsx
@@ -1,5 +1,14 @@
 import "./error.css";
+const ErrorImg = require("../assets/magnifying-glass.jpg");
 
 export default function Error() {
-  return <div className="main"></div>;
+  return (
+    <div className="main">
+      <div className="content">
+        <h1>Sorry</h1>
+        <span>We couldn't find that page</span>
+      </div>
+      <img src={String(ErrorImg)} alt="Error Image" className="error-img" />
+    </div>
+  );
 }
