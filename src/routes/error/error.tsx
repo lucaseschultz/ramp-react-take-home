3e79edb6be2f49bcdb4026f22c5e3df3e40d1b7c
Lucas Schultz <156949026+lucaseschultz@users.noreply.github.com>
2024-09-05 18:53:03 +0000

    changed error pages' classes from generic (".main") to specific (".error-main") to prevent unwanted styling

--- src/routes/error/error.tsx
+++ src/routes/error/error.tsx
@@ -3,8 +3,8 @@ const ErrorImg = require("./assets/magnifying-glass.jpg");
 
 export default function Error() {
   return (
-    <div className="main">
-      <div className="content">
+    <div className="error-main">
+      <div className="error-content">
         <h1>Sorry</h1>
         <span>We couldn't find that page</span>
       </div>
