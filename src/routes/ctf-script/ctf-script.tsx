e374716c0f69bdfd4a5fdac690b79cffc2302f7e
Lucas Schultz <156949026+lucaseschultz@users.noreply.github.com>
2024-09-05 13:25:59 +0000

    Added template for ctf-script page

--- src/routes/ctf-script/ctf-script.tsx
+++ src/routes/ctf-script/ctf-script.tsx
@@ -1,5 +1,11 @@
 import "./ctf-script.css";
 
 export default function CTFScript() {
-  return <div className="main">{/* insert script here */}</div>;
+  return (
+    <div className="main">
+      <div className="content">
+        <div className="code-container">{/* insert script here */}</div>
+      </div>
+    </div>
+  );
 }
