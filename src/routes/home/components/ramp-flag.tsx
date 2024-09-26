5001170334f2d2d487a586c10c36a933b8c5edb7
lucas <lucaseschultz@gmail.com>
2024-09-26 12:32:18 -0600

    changed ramp-flag-components file to ramp-flag_components

--- src/routes/home/components/ramp-flag.tsx
+++ src/routes/home/components/ramp-flag.tsx
@@ -1,7 +1,7 @@
 import "./ramp-flag.css";
 import { Suspense } from "react";
-import LoadingFlag from "./ramp-flag-components/loading-flag";
-import LoadedFlag from "./ramp-flag-components/loaded-flag";
+import LoadingFlag from "./ramp-flag_components/loading-flag";
+import LoadedFlag from "./ramp-flag_components/loaded-flag";
 
 export default function RampFlag() {
   return (
